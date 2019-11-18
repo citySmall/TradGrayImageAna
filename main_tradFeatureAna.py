3db5bfc96963d996cd549b2da90c3d41242b2ecb
tao xu <WhuXuTao@outlook.com>
2019-11-18 18:18:47 +0800

    test on some examples

--- main_tradFeatureAna.py
+++ main_tradFeatureAna.py
@@ -17,28 +17,34 @@ def f_3(x, A, B, C, D):
 
 
 def f_4(x, A, B, C, D, E):
-    return A*x*x*x*x + B*x*x*x + C*x*x + D*x + E;
+    return A*x*x*x*x + B*x*x*x + C*x*x + D*x + E
+
+
+def df_4 (x, A, B, C, D, E):
+    return A * x * x * x * 4 + B * x * x * 3 + C * x * 2 + D;
 
 
 def feature_ana(tiff_path, save_path, delta = 1):
-    plt.figure(figsize=(20,14))
+    plt.figure(figsize=(5,5))
     # tif = TiffFile(tiff_path)
-    image = cv2.imread(tiff_path, 0)[200:, :]
+    image = cv2.imread(tiff_path, 0)[340:, :]
+    mean, std = np.mean(image), np.std(image)
     image = cv2.medianBlur(image, 7)
-    image_arr = image[:, :30]
+    image_arr = np.mean(image[:, 2210:2220], axis=1)
     print('shape:{}'.format(image_arr.shape))
-    row, column = math.ceil(image_arr.shape[0]/delta), image_arr.shape[1]
+    row, column = math.ceil(image_arr.shape[0])-delta, 10
     image_arr = image_arr.astype("float")
 
-    gradient = np.zeros((row, column))
+    gradient = np.zeros((row-1, column))
 
-    x = np.arange(0, image_arr[:, 0].shape[0], 1)
-    popt, pcov = optimize.curve_fit(f_4, x, image_arr[:, 0], [1, 1, 1, 1, 1])
-    y_fit = f_4(x, popt[0], popt[1], popt[2], popt[3], popt[4])
+    x = np.arange(0, image_arr.shape[0], 1)
+    popt, pcov = optimize.curve_fit(f_4, x, image_arr, [1, 1, 1, 1, 1])
+    y = f_4(x, popt[0], popt[1], popt[2], popt[3], popt[4])
+    y_fit = df_4(x, popt[0], popt[1], popt[2], popt[3], popt[4])
 
-    for x in range(delta, row, delta):
-        gx = abs(y_fit[x - delta] - y_fit[x]) / delta
-        gradient[x, 0] = gx  # + gy
+    for x in range(delta, row):
+        gx = (y[x] - y[x - delta]) / delta
+        gradient[x-1, 0] = gx
         # for y in range(1):
         #     gx = abs(moon_f[x - delta, y] - moon_f[x, y]) / delta
         #     # gy = abs(moon_f[x, y + 1] - moon_f[x, y])
@@ -46,11 +52,17 @@ def feature_ana(tiff_path, save_path, delta = 1):
 
     plt.subplot('311')
     plt.plot(gradient[:, 0])
-    plt.subplot('312')
     plt.plot(y_fit)
+    plt.plot(range(0, 600, 20), [0]*30, marker='.', markersize=0.01, linestyle=':')
+    plt.title("cancer, avg: {}--std: {}".format(int(mean), round(std, 2)))
+    plt.subplot('312')
+    plt.plot(y)
+    plt.plot(image_arr)
     plt.subplot('313')
     plt.imshow(image, cmap='gray')
+    plt.savefig('./results/hsil2-avg-2210-2220.png')
     plt.show()
+    print(gradient[:,0])
     # sharp = moon_f + gradient
     # sharp = np.where(sharp < 0, 0, np.where(sharp > 255, 255, sharp))
     #
@@ -69,7 +81,7 @@ def feature_ana(tiff_path, save_path, delta = 1):
         arr = cv2.medianBlur(image_arr[i, :, :], ksize=1)
         dys.append(cv2.Sobel(arr,cv2.CV_32F,0, 1, ksize=1))
     plt.subplot(rows, cols, 1)
-    plt.imshow(image_arr[1].T[:600, :], cmap="gray")
+    plt.imshow(image_arr[1].T[:, :], cmap="gray")
     maxidx = 0
     sum = 0.0
     for i in range(1, image_arr.shape[0] + 1):
@@ -107,6 +119,8 @@ if __name__ == "__main__":
         for tp in tiff_paths:
             print(tp)
             save_name = prefix[idx] + "-" + tp[:-5] + '.png'
-            feature_ana(os.path.join(inner_dir, tp), os.path.join(save_dir, save_name))
+            # feature_ana(os.path.join(inner_dir, tp), os.path.join(save_dir, save_name))
+            feature_ana('./images/hsil/B1903834-11_circle_5.0x5.0_C01_S0009_0.png',
+                        os.path.join(save_dir, save_name))
             break
         break
