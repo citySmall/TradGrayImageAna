6f2e67728c8aa4777282f33f6726a99e7582993e
tao xu <WhuXuTao@outlook.com>
2019-11-20 18:07:15 +0800

    -_-

--- main_tradFeatureAna.py
+++ main_tradFeatureAna.py
@@ -24,14 +24,51 @@ def df_4 (x, A, B, C, D, E):
     return A * x * x * x * 4 + B * x * x * 3 + C * x * 2 + D;
 
 
-def feature_ana(tiff_path, save_path, delta = 1):
-    plt.figure(figsize=(5,5))
+def feature_ana(tiff_path, save_path, delta = 1, fs = 21):
+    # plt.figure(figsize=(5,5))
     # tif = TiffFile(tiff_path)
-    image = cv2.imread(tiff_path, 0)[340:, :]
+    image = cv2.imread(tiff_path, 0)[:, :]
     mean, std = np.mean(image), np.std(image)
-    image = cv2.medianBlur(image, 7)
-    image_arr = np.mean(image[:, 2210:2220], axis=1)
+    image = cv2.blur(image, (9, 9))
+    image_arr = np.transpose(np.asarray([np.mean(image[:, m:m+10], axis=1) for m in range(0, image.shape[-1], 10)]))
     print('shape:{}'.format(image_arr.shape))
+
+    gap_grads = []
+    for i in range(image_arr.shape[-1]):
+        high_idx = -1
+        low_idx = -1
+        max_in = np.max(image_arr[:, i])
+        for j in range(image_arr.shape[0]-20):
+            if np.mean(image_arr[j:j+5, i]) > 0.9 * max_in and high_idx == -1:
+                high_idx = j
+                for k in range(j, image_arr.shape[0]-20):
+                    if np.mean(image_arr[k:k + 20, i]) < 0.2 * max_in and low_idx == -1:
+                        low_idx = k
+                        break
+                break
+        if high_idx == -1:
+            high_idx = 0
+        if low_idx == -1:
+            low_idx = image_arr.shape[0]-20
+        gap_grads.append((np.mean(image_arr[high_idx:high_idx+5]) - np.mean(image_arr[low_idx:low_idx+20]))
+                         / (low_idx - high_idx))
+        high = np.mean(image_arr[high_idx:high_idx+5])
+        low = np.mean(image_arr[low_idx:low_idx+20])
+        print("i:{}-high_idx:{}-low-idx:{}-high-{}-low-{}".format(i, high_idx, low_idx, high, low))
+
+
+
+    plt.subplot('311')
+    plt.plot(image_arr[:, 20])
+    plt.ylim(50,256)
+    plt.subplot('312')
+    plt.plot(gap_grads)
+    plt.subplot('313')
+    plt.imshow(image_arr, cmap='gray')
+    plt.show()
+
+    return
+
     row, column = math.ceil(image_arr.shape[0])-delta, 10
     image_arr = image_arr.astype("float")
 
@@ -60,7 +97,7 @@ def feature_ana(tiff_path, save_path, delta = 1):
     plt.plot(image_arr)
     plt.subplot('313')
     plt.imshow(image, cmap='gray')
-    plt.savefig('./results/hsil2-avg-2210-2220.png')
+    plt.savefig('./results/normal3-avg-0-10.png')
     plt.show()
     print(gradient[:,0])
     # sharp = moon_f + gradient
@@ -120,7 +157,7 @@ if __name__ == "__main__":
             print(tp)
             save_name = prefix[idx] + "-" + tp[:-5] + '.png'
             # feature_ana(os.path.join(inner_dir, tp), os.path.join(save_dir, save_name))
-            feature_ana('./images/hsil/B1903834-11_circle_5.0x5.0_C01_S0009_0.png',
+            feature_ana('./images/normal/M0003_2019_P0000093_circle_2.0x2.0_C04_S004.png',
                         os.path.join(save_dir, save_name))
             break
         break
